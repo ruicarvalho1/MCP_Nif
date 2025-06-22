2b4230cb7694f6eda56e192d3cb4f7065d65a9b1
Rui  Carvalho <95130928+ruicarvalho1@users.noreply.github.com>
2025-06-22 15:35:06 +0100

    Dynamic api key

--- nif_client.py
+++ nif_client.py
@@ -2,18 +2,17 @@ import httpx
 from urllib.parse import quote
 
 NIFPT_API_URL = "https://www.nif.pt/?json=1&q="
-API_KEY = "98eda15d86624a3b57223c7dde7dba50"
 
-async def fetch_company_by_nif(nif: str):
-    url = f"{NIFPT_API_URL}{nif}&key={API_KEY}"
+async def fetch_company_by_nif(nif: str, api_key: str):
+    url = f"{NIFPT_API_URL}{nif}&key={api_key}"
     async with httpx.AsyncClient() as client:
         resp = await client.get(url)
         resp.raise_for_status()
         return resp.json()
 
-async def fetch_companies_by_term(term: str):
+async def fetch_companies_by_term(term: str, api_key: str):
     formatted_term = quote(term)
-    url = f"{NIFPT_API_URL}{formatted_term}&key={API_KEY}"
+    url = f"{NIFPT_API_URL}{formatted_term}&key={api_key}"
     async with httpx.AsyncClient() as client:
         resp = await client.get(url)
         resp.raise_for_status()
